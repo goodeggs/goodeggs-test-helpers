bf7a3e19ccac1d59b4b43f38a1afb3d8de668748
Max Edmands <git@maxedmands.com>
2016-09-13 12:27:48 -0700

    more logging!

--- test/exec_file_as_test.js
+++ test/exec_file_as_test.js
@@ -6,12 +6,13 @@ import path from 'path';
 
 describe('exec_file_as_test', function () {
   it('allows you to run a js file and have it interpereted as a test', function (done) {
-    this.timeout(5000);
+    this.timeout(10000);
 
     const file = path.join(__dirname, 'simpletest.js');
     const proc = cp.spawn('babel-node', [file]);
     let seenMagicalText = false;
     proc.stderr.pipe(process.stderr);
+    proc.stdout.pipe(process.stdout);
     proc.stdout.on('data', function (d) {
       if (d.toString('utf8').match(/does not test anything in particular/))
         seenMagicalText = true;
