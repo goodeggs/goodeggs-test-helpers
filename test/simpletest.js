51f682bc1bf408dd7053f66250c1e30ad2f86822
Max Edmands <git@maxedmands.com>
2016-09-13 12:36:56 -0700

    even MORE logging

--- test/simpletest.js
+++ test/simpletest.js
@@ -1,6 +1,7 @@
 /* eslint-env goodeggs/server-side-test */
 import '../src';
 
+console.error('inside the simple test');
 describe('simple test', function () {
   it('does not test anything in particular, but is used by exec_file_as_test.js', function () {});
 });
