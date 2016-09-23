be582bad3555c5f36be928c1b663f3e6bf1a017d
Max Edmands <git@maxedmands.com>
2016-09-23 09:47:41 -0700

    Add withContext to flow helpers

--- src/flow.js
+++ src/flow.js
@@ -7,6 +7,7 @@ declare function before(title?: string, fn: (cb?:(err?:Error) => void) => mixed)
 declare function beforeEach(title?: string, fn: (cb?:(err?:Error) => void) => mixed): void;
 declare function after(title?: string, fn: (cb?:(err?:Error) => void) => mixed): void;
 declare function afterEach(title?: string, fn: (cb?:(err?:Error) => void) => mixed): void;
+declare function withContext(name: string, val: mixed): void;
 
 declare function it(title: string, fn?: (cb?:(err?:Error) => void) => mixed): void;
 
