bcdbe298c7c55e4e8f23a3158adb163c4c96bd73
Max Edmands <git@maxedmands.com>
2017-02-15 19:10:31 -0800

    More accurate flow test declarations

--- src/flow.js
+++ src/flow.js
@@ -1,13 +1,22 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-redeclare */
 
 declare function describe(title: string, fn: () => void): void;
 declare function given(title: string, fn: () => void): void;
 declare function context(title: string, fn: () => void): void;
 
-declare function before(title?: string, fn: (cb?:(err?:Error) => void) => mixed): void;
-declare function beforeEach(title?: string, fn: (cb?:(err?:Error) => void) => mixed): void;
-declare function after(title?: string, fn: (cb?:(err?:Error) => void) => mixed): void;
-declare function afterEach(title?: string, fn: (cb?:(err?:Error) => void) => mixed): void;
+type possiblyAsyncFunction =
+  ((cb: (err?:Error) => void) => void) |
+  (() => (void | Promise<any>));
+
+declare function before(title: string, fn: possiblyAsyncFunction): void;
+declare function before(fn: possiblyAsyncFunction): void;
+declare function beforeEach(title: string, fn: possiblyAsyncFunction): void;
+declare function beforeEach(fn: possiblyAsyncFunction): void;
+declare function after(title: string, fn: possiblyAsyncFunction): void;
+declare function after(fn: possiblyAsyncFunction): void;
+declare function afterEach(title: string, fn: possiblyAsyncFunction): void;
+declare function afterEach(fn: possiblyAsyncFunction): void;
 declare function withContext(name: string, val: mixed): void;
 
 declare function it(title: string, fn?: (cb?:(err?:Error) => void) => mixed): void;
