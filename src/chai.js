00f2232cdeefe1f33fffafe253740cf7d4628bdf
Max Edmands <git@maxedmands.com>
2017-03-03 11:52:26 -0800

    Add chai-json-schema for JSON schema validation

--- src/chai.js
+++ src/chai.js
@@ -4,12 +4,14 @@ import chaid from 'chaid';
 import dirtyChai from 'dirty-chai';
 import sinonChai from 'sinon-chai';
 import chaiDateTime from 'chai-datetime';
+import chaiJSONSchema from 'chai-json-schema';
 
 chai.use(chaiAsPromised);
 chai.use(dirtyChai);
 chai.use(sinonChai);
 chai.use(chaid);
 chai.use(chaiDateTime);
+chai.use(chaiJSONSchema);
 
 // To add more chai plugins, require chai from this file.
 export default chai;
