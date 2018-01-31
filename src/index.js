00f1cd922def3cbb1702361c6ae27d3f08702074
Danny Nelson <danny@goodeggs.com>
2018-01-30 19:07:18 -0800

    Simplify test setup
    Changes:
    - Remove support for `babel-node`ing a test file - simplifies interface and enforces single standard for running tests.
    - Export all helpers from `index.js` - No more reaching into the module (e.g. `goodeggs-test-helpers/chai`)
    - Move `./run_tests.sh` from `goodeggs-ops-api-test-helpers` so we can use it for modules too
    - Remove `withContext()` - prefer mocha's `beforeEach` instead
    - Remove `given()` - prefer mocha's `context` instead
    - Publish from root of project
    - Export flow coverage for mocha/chai from flow-typed - via copy paste, `flow gen-flow-files` didn't work
    
    Before:
    ```
    babel-node /path/to/test.js
    ```
    ```js
    /* eslint-env goodeggs/server-side-test */ // hack due to test globals
    import 'goodeggs-ops-api-test-helpers/server' // magically creates hooks and allows babel-noding this file
    
    import sinon from 'sinon' // may be a different instance of sinon than the sandbox
    
    describe ('test', function () {  // globals! no flow coverage
      // ...
    })
    ```
    
    after:
    ```
    run-tests /path/to/test.js
    ```
    ```js
    import {describe, it, expect, sinon, useSinonSandbox} from 'goodeggs-test-helpers'; // all covered by flow
    
    describe ('test', function () {
      useSinonSandbox(); // Explicit test setup in the same place we define other test hooks. No magic.
    })
    ```

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import './mocha_context.js';
-import './globals';
-import './chai';
+// @flow
+// TODO, ideally we would export {desribe, it} etc from mocha.
+// But those are not available when imported into thirdparty dependencies :(
+export {default as sinon} from 'sinon';
+export {expect} from './chai';
+export {default as useSinonSandbox} from './use_sinon_sandbox';
--- test/simpletest.js
+++
@@ -1,6 +0,0 @@
-/* eslint-env goodeggs/server-side-test */
-import '../build';
-
-describe('simple test', function () {
-  it('does not test anything in particular, but is used by exec_file_as_test.js', function () {});
-});
