ed0fde7848fd62d985b141bbe17e82a1d7cca345
Danny Nelson <danny@goodeggs.com>
2018-01-31 20:28:17 -0800

    Improve runtest
    - Add --exit to simplify mocha upgrade path
    - Bump to alpha instanbul so that it works with babel

--- runtest.sh
+++ runtest.sh
@@ -5,14 +5,15 @@ export NODE_ENV=test
 export BLUEBIRD_DEBUG=1
 export multi='mocha-osx-reporter=- dot=-'
 
-MOCHA_ARGS="--require=babel-register"
+# TODO: remove --exit once all apps shutdown tests gracefully
+MOCHA_ARGS="--exit --require=babel-register"
 MOCHA_CMD="./node_modules/mocha/bin/mocha $MOCHA_ARGS"
 ISTANBUL_CMD="babel-node ./node_modules/istanbul/lib/cli cover --dir=.coverage ./node_modules/mocha/bin/_mocha -- $MOCHA_ARGS"
 
 case "$1" in
   --watch) $MOCHA_CMD --watch --reporter=mocha-multi "${@: -1}"
     ;;
-  --coverage) $ISTANBUL_CMD
+  --coverage) $ISTANBUL_CMD "${@: -1}"
     ;;
   *) $MOCHA_CMD "${@: -1}"
 esac
