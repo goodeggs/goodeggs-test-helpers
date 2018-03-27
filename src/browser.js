317fe5d131bd13d5a2a3464d0935e93ead5d2b8f
Philippe Serhal <philippe@goodeggs.com>
2018-03-27 16:09:36 -0700

    Export chai so consumers can use additional plugins.
    Also make it clear that we're testing built code, and avoid confusing flow errors before building.

--- src/browser.js
+++ src/browser.js
@@ -1,4 +1,7 @@
 // @flow
+import chai from './chai';
+
+const {expect} = chai;
+export {expect, chai};
 export {default as sinon} from 'sinon';
 export {default as useSinonSandbox} from './use_sinon_sandbox';
-export {expect} from './chai';
--- test/other_test.js
+++ test/other_test.js
@@ -1,7 +1,7 @@
 // @flow
 import {describe, it} from 'mocha';
 
-import {expect} from '../';
+import {expect} from '../lib';
 
 describe('other', function () {
   it('works', function () {
