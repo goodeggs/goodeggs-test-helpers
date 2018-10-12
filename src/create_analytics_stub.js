573883f6896f5eabc585dcd5c398b2f07f440c1c
Nathan Houle <nathan@nathanhoule.com>
2018-10-11 22:46:38 -0700

    Replace analytics.js stub with better type

--- src/create_analytics_stub.js
+++ src/create_analytics_stub.js
@@ -1,24 +1,23 @@
 // @flow
-// TODO(ndhoule): Replace with a better type
 type Analytics = {
-  alias: Function,
-  group: Function,
-  identify: Function,
-  page: Function,
-  screen: Function,
-  track: Function,
+  alias ({previousId: string, userId: string}): void;
+  group ({groupId: string, traits?: {}}): void;
+  identify ({userId: string, traits?: {}}): void;
+  page ({name: string, properties?: {}}): void;
+  screen ({name: string, properties?: {}}): void;
+  track ({event: string, properties?: {}}): void;
 };
 
 /**
  * Creates an analytics.js (window.analytics)-compatible stub.
  */
 const createAnalyticsStub = (overrides?: $Rest<Analytics, {}>): Analytics => ({
-  alias: () => {},
-  group: () => {},
-  identify: () => {},
-  page: () => {},
-  screen: () => {},
-  track: () => {},
+  alias: (() => {}: $PropertyType<Analytics, 'alias'>),
+  group: (() => {}: $PropertyType<Analytics, 'group'>),
+  identify: (() => {}: $PropertyType<Analytics, 'identify'>),
+  page: (() => {}: $PropertyType<Analytics, 'page'>),
+  screen: (() => {}: $PropertyType<Analytics, 'screen'>),
+  track: (() => {}: $PropertyType<Analytics, 'track'>),
 
   ...overrides,
 });
