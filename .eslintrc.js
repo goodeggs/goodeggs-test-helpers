e665aff30c6109ac2d43be4c95a4e6dcc380487a
Polo Ma <42830316+pma9@users.noreply.github.com>
2021-02-02 14:46:50 -0800

    Address minor PR comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,23 @@ module.exports = {
   },
   overrides: [
     {
+      files: ['**/{*.,}test{.*,}.{js,jsx,ts,tsx}'],
+      extends: ['plugin:goodeggs/mocha'],
+      env: {
+        mocha: true,
+      },
+    },
+    {
       files: ['src/use_sinon_sandbox.js'],
       env: {
         mocha: true,
       },
     },
     {
-      files: ['babel.config.js'],
+      files: ['*.config{.babel,}.js', '.*rc.js'],
+      env: {
+        node: true,
+      },
       rules: {
         'import/no-commonjs': 'off',
       },
