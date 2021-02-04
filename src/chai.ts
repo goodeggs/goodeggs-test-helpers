e7c4f42d84379ec7f7cfeec93fb416a8bd0e0f9d
Polo Ma <42830316+pma9@users.noreply.github.com>
2021-02-04 10:17:41 -0800

    Remove chaid package

--- src/chai.ts
+++ src/chai.ts
@@ -1,6 +1,5 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import chaid from 'chaid';
 import dirtyChai from 'dirty-chai';
 import sinonChai from 'sinon-chai';
 import chaiDateTime from 'chai-datetime';
@@ -8,7 +7,6 @@ import chaiDateTime from 'chai-datetime';
 chai.use(chaiAsPromised);
 chai.use(dirtyChai);
 chai.use(sinonChai);
-chai.use(chaid);
 chai.use(chaiDateTime);
 
 export default chai;
--- test/test.ts
+++ test/test.ts
@@ -24,13 +24,6 @@ describe('mocha helpers', function () {
         dateTestHelpers.pacific.time('2015-04-01 00:00:00'),
       );
     });
-
-    it('has chai-id', function () {
-      expect({_id: 'foo'}).to.have.id('foo');
-      expect([{id: 'a'}, {id: 'b'}]).to.have.ids(['a', 'b']);
-      expect([{id: 'a'}, {id: 'b'}]).to.have.same.unordered.ids([{id: 'b'}, {id: 'a'}]);
-      expect([{id: 'a'}, {id: 'b'}]).to.include.same.ids([{id: 'a'}]);
-    });
   });
 
   describe('useSinonSandbox()', function () {
