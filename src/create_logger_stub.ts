fbdab6940c0d1e81c9ea18b1a4b0b3d33f7af89b
Philippe Serhal <philippe@goodeggs.com>
2021-03-04 15:40:42 -0500

    types: tweak logger stub type and clarify a comment

--- src/create_logger_stub.ts
+++ src/create_logger_stub.ts
@@ -1,14 +1,15 @@
 // `goodeggs-server/logger`-compatible interface
-// TODO Replace with actual goodeggs-logger type once it exists
-export type Logger = {
-  child(opts: {[key: string]: unknown}): Logger;
+// Since this is a public module and goodeggs-logger is private, we can't reference its type.
+// Instead, limit this type to the minimum that this library actually needs to know.
+export interface Logger {
+  child(...params: Array<unknown>): Logger;
   debug(...params: Array<unknown>): void;
   error(...params: Array<unknown>): void;
   fatal(...params: Array<unknown>): void;
   info(...params: Array<unknown>): void;
   trace(...params: Array<unknown>): void;
   warn(...params: Array<unknown>): void;
-};
+}
 
 /**
  * Creates a goodeggs-server/Logger-compatible stub.
