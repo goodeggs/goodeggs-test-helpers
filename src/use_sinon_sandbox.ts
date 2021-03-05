2c84b08b0d5b612193d41ceb6efe569e6ac80f15
Philippe Serhal <philippe@goodeggs.com>
2021-03-05 11:51:10 -0500

    docs: remove misleading comment
    There's no reason why this wouldn't work in Jest.

--- src/use_sinon_sandbox.ts
+++ src/use_sinon_sandbox.ts
@@ -16,8 +16,6 @@ interface StubLoggerReturn {
 /**
  * Create a Sinon sandbox that resets itself between each test. Also returns a goodeggs-logger stub
  * that is stubbed using the returned sandbox.
- *
- * NOTE: Only use this in a Mocha environment, it's not guaranteed to work in Jest.
  */
 export default function useSinonSandbox(): {
   sandbox: SinonSandbox;
