76fe3aa69bed3aa97b05d5061a19296f56f0b2a1
Philippe Serhal <philippe@goodeggs.com>
2021-03-05 11:49:51 -0500

    feat: return an object of stubs from stubLogger()
    Otherwise, consumers have to reference e.g. `logger.error.callCount`, which introduces
    TypeScript type errors. Now they can just go `const {error} = stubLogger(logger); error.callCount`
    or whatever.

--- src/use_sinon_sandbox.ts
+++ src/use_sinon_sandbox.ts
@@ -1,7 +1,17 @@
-import sinon, {SinonSandbox} from 'sinon';
+import sinon, {SinonStub, SinonSandbox} from 'sinon';
 import assert from 'assert';
 
 import {Logger} from './create_logger_stub';
+
+interface StubLoggerReturn {
+  trace: SinonStub;
+  debug: SinonStub;
+  info: SinonStub;
+  warn: SinonStub;
+  error: SinonStub;
+  fatal: SinonStub;
+  child: SinonStub;
+}
 
 /**
  * Create a Sinon sandbox that resets itself between each test. Also returns a goodeggs-logger stub
@@ -15,15 +25,18 @@ export default function useSinonSandbox(): {
 } {
   const sandbox = sinon.createSandbox();
 
-  const stubLogger = (logger: Logger) => {
+  const stubLogger = (logger: Logger): StubLoggerReturn => {
     assert(logger != null, 'logger required');
-    sandbox.stub(logger, 'trace');
-    sandbox.stub(logger, 'debug');
-    sandbox.stub(logger, 'info');
-    sandbox.stub(logger, 'warn');
-    sandbox.stub(logger, 'error');
-    sandbox.stub(logger, 'fatal');
-    sandbox.stub(logger, 'child').returns(logger);
+
+    const trace = sandbox.stub(logger, 'trace');
+    const debug = sandbox.stub(logger, 'debug');
+    const info = sandbox.stub(logger, 'info');
+    const warn = sandbox.stub(logger, 'warn');
+    const error = sandbox.stub(logger, 'error');
+    const fatal = sandbox.stub(logger, 'fatal');
+    const child = sandbox.stub(logger, 'child').returns(logger);
+
+    return {trace, debug, info, warn, error, fatal, child};
   };
 
   afterEach(function () {
